f160cb8de5ec24543ffbd302a238628436ea21aa
Abdullah Zayed <41540861+xaadu@users.noreply.github.com>
2020-11-27 02:30:26 +0600

    Completed

--- modal/script.js
+++ modal/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+// DOM COMPONENTS
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+    btnsShowModal[i].addEventListener('click', openModal)
+};
+
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) { // keypress event doesn't work for Escape Key
+    if (e.key === 'Escape') closeModal();
+});
